517284435bc46fda3d11328c36f826f901092f8a
DavidNgo_Dev <13045581+DavidNgoDev@users.noreply.github.com>
2019-01-19 14:35:17 +1100

    Add files via upload

---
+++ audit.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+sudo apt-get install chkrootkit
+sudo apt-get install rkhunter
+audo apt-get install lynis
+wget https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh -O les.sh -P /tmp
+wget https://meltdown.ovh -O spectre-meltdown-checker.sh -P /tmp
+
+sudo chkrootkit
+sudo rkhunter -c
+sudo lynis -c
+chmod +x /tmp/spectre-meltdown-checker.sh
+sudo ./spectre-meltdown-checker.sh
+chmod +x /tmp/les.sh
+./les.sh
+
