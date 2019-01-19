438e181f70f2d228e037fbe71dc6ec788e220a97
DavidNgo_Dev <13045581+DavidNgoDev@users.noreply.github.com>
2019-01-19 14:48:41 +1100

    Update audit.sh

--- audit.sh
+++ audit.sh
@@ -8,8 +8,8 @@ wget https://meltdown.ovh -O spectre-meltdown-checker.sh -P /tmp
 sudo chkrootkit
 sudo rkhunter -c
 sudo lynis -c
-chmod +x /tmp/spectre-meltdown-checker.sh
+sudo chmod +x /tmp/spectre-meltdown-checker.sh
 sudo ./spectre-meltdown-checker.sh
-chmod +x /tmp/les.sh
-./les.sh
+sudo chmod +x /tmp/les.sh
+sudo ./les.sh
 
